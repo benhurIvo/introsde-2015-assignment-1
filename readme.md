0e5ae841d0109e0f35a811e6c871604204a3d755
benhurIvo <benhurivo@gmail.com>
2015-11-11 14:52:54 +0300

    third commit

--- readme.md
+++ readme.md
@@ -2,40 +2,25 @@ Kayongo Ivan
 181511
 
 
-This code is an evaluatioon of xPath and JAXB XJC. The XPath part prints values from an xml database of 50 people ie xpathDB.xml, it can print a person's details using his ID, and also their weight and height.
-The second part ie JAXB XJC does the marshalling and un-marshalling of an XML file, and also creates a JSON database.
-
+This code is an evaluatioon of xPath and JAXB XJC. 
+There are two folders under the src folder, "utils" and "sde", utils has java classes that have methods used to generate random values for use in other classes.
+The "sde" folder the main classes which do all the work ie 
+-XPath class which prints values from an xml database of 50 people ie xpathDB.xml, print a person's details using his ID, and also their weight and height.
+-Marshalling and un-marshalling classes of an XML file, and also a class that creates a JSON database.
+There is also an 'xsd' file which is used in the Marshalling and un-marshalling classes.
 
 How to run it.
-On the commandline termonal, change directory to the folder of the project ie "sde"
+On the commandline terminal, change directory to the folder of the project ie "sde"
 
 compile using ant; ie run
 
 ant execute.evaluation
 
+This will compile the project and download all the necessary dependences and then print the following
 
-This will compile the project and download all the necessary dependences.
-
-For XPath, run 
-
-ant execute.HPReader
-
-This prints three things ie
 1. All the people in the people.xml database
 2. The health profile of the person with ID 5
 3. All the people with weight over 90 kilograms
-
-
-For JAXB XJC, three commands are used ie
-
-1. ant execute.JAXBMarshaller
-	
-	This creates an xml file called people.xml with 4 people and their details
-	
-2. ant execute.JAXBUnMarshaller
-	
-	This command gets details from the people.xml file created in (1) above and prints them
-	
-3. ant execute.HPJson
-	
-	The command creates a JSON database of 4 people
+4. Creates an xml file called people.xml with 4 people and their details
+5. Gets details from the people.xml file created in (1) above and prints them
+6. Creates a JSON database of 4 people
--- src/sde/XPathPrinter.java
+++ src/sde/XPathPrinter.java
@@ -1,4 +1,5 @@
 package sde;
+
 /**
  * Based on code made by Muhammad Imran
  */
@@ -14,6 +15,7 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -24,105 +26,126 @@ public class XPathPrinter {
     XPath xpath;
     DocumentBuilder builder;
     DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-	    
+
     public void loadXML() throws ParserConfigurationException, SAXException, IOException {
 
-        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-        domFactory.setNamespaceAware(true);
-        builder = domFactory.newDocumentBuilder();
-        doc = builder.parse("xpathDB.xml");
+	DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+	domFactory.setNamespaceAware(true);
+	builder = domFactory.newDocumentBuilder();
+	doc = builder.parse("xpathDB.xml");
 
-        //creating xpath object
-        getXPathObj();
+	//creating xpath object
+	getXPathObj();
     }
 
     public XPath getXPathObj() {
 
-        XPathFactory factory = XPathFactory.newInstance();
-        xpath = factory.newXPath();
-        return xpath;
+	XPathFactory factory = XPathFactory.newInstance();
+	xpath = factory.newXPath();
+	return xpath;
     }
 
     // Get Weight of person with that id
     public Node getWeight(long id) throws XPathExpressionException {
 
-        XPathExpression expr = xpath.compile("/people/person[@id ="+id+"]/healthprofile/weight");
-        Node node = (Node) expr.evaluate(doc, XPathConstants.NODE);
-        return node;
+	XPathExpression expr = xpath.compile("/people/person[@id =" + id + "]/healthprofile/weight");
+	Node node = (Node) expr.evaluate(doc, XPathConstants.NODE);
+	return node;
     }
 
     // Get Height of person with that id
-public Node getHeight(long id) throws XPathExpressionException {
+    public Node getHeight(long id) throws XPathExpressionException {
 
-        XPathExpression expr = xpath.compile("/people/person[@id ="+id+"]/healthprofile/height");
-        Node node = (Node) expr.evaluate(doc, XPathConstants.NODE);
-        return node;
+	XPathExpression expr = xpath.compile("/people/person[@id =" + id + "]/healthprofile/height");
+	Node node = (Node) expr.evaluate(doc, XPathConstants.NODE);
+	return node;
     }
 
 // Print all the people in the database
-  public NodeList printPeople() throws XPathExpressionException,SAXException,IOException,ParserConfigurationException {
+    public void printPeople() throws XPathExpressionException, SAXException, IOException, ParserConfigurationException {
 
-      XPathExpression expr = xpath.compile("/people");
-        doc = builder.parse("xpathDB.xml");
+	String expresn = "/people";
+	doc = builder.parse("xpathDB.xml");
+//Get root node from our expression
+	Node node = (Node) xpath.compile(expresn).evaluate(doc, XPathConstants.NODE);
 
-	    Object result = expr.evaluate(doc, XPathConstants.NODESET);
-	    NodeList nodes = (NodeList) result;
-	    for (int i = 0; i < nodes.getLength(); i++) {
-	        System.out.println(nodes.item(i).getTextContent());
-	    }
-	    return null;
+//Child nodes under the root node as a list
+	NodeList c = node.getChildNodes();
+
+	printResults(c);
     }
-  
-  // Pring health profile of person with given ID
-  public NodeList printHealthProfile(long id) throws XPathExpressionException,SAXException,IOException,ParserConfigurationException {
 
-      XPathExpression expr = xpath.compile("/people/person[@id ="+id+"]");
-        doc = builder.parse("xpathDB.xml");
-	    Object result = expr.evaluate(doc, XPathConstants.NODESET);
-	    NodeList nodes = (NodeList) result;
-	    for (int i = 0; i < nodes.getLength(); i++) {
-	        System.out.println(nodes.item(i).getTextContent());
-	    }
-	    return null;
-    }
-  
-  public NodeList calcWeight(String wt) throws XPathExpressionException,SAXException,IOException,ParserConfigurationException {
+    // Print health profile of person with given ID
+    public void printHealthProfile(long id) throws XPathExpressionException, SAXException, IOException, ParserConfigurationException {
 
-      XPathExpression expr = xpath.compile("/people/person[healthprofile/weight"+wt+"]");
-        doc = builder.parse("xpathDB.xml");
-	    Object result = expr.evaluate(doc, XPathConstants.NODESET);
-	    NodeList nodes = (NodeList) result;
-	    for (int i = 0; i < nodes.getLength(); i++) {
-	        System.out.println(nodes.item(i).getTextContent());
-	    }
-	    return null;
+	XPathExpression expr = xpath.compile("/people/person[@id =" + id + "]");
+	doc = builder.parse("xpathDB.xml");
+	Object result = expr.evaluate(doc, XPathConstants.NODESET);
+	printResults((NodeList) result);
+
     }
-   
+
+    public void calcWeight(String wt) throws XPathExpressionException, SAXException, IOException, ParserConfigurationException {
+
+	XPathExpression expr = xpath.compile("/people/person[healthprofile/weight" + wt + "]");
+	doc = builder.parse("xpathDB.xml");
+	Object result = expr.evaluate(doc, XPathConstants.NODESET);
+	printResults((NodeList) result);
+    }
+
+//This method prints results of an expression passed to it from the xml file 
+    public void printResults(NodeList c) {
+	for (int i = 0; i < c.getLength(); i++) {
+	    if (c.item(i) instanceof Element) {
+		NodeList children = c.item(i).getChildNodes();
+		//Print the person's id
+		System.out.println("No: " + c.item(i).getAttributes().getNamedItem("id").getTextContent());
+		for (int j = 0; j < children.getLength(); j++) {
+		    if (children.item(j) instanceof Element) {
+			//Check if child node is not under healthprofile
+			if (!((Element) children.item(j)).getTagName().contains("healthprofile")) {
+			    System.out.println(((Element) children.item(j)).getTagName() + " : " + children.item(j).getTextContent());
+			} //Check if child node is healthprofile node
+			else {
+			    System.out.println("Healthprofile:");
+			    NodeList nl = ((Element) children.item(j)).getChildNodes();
+
+			    for (int k = 0; k < nl.getLength(); k++) {
+				if (!nl.item(k).getNodeName().contains("#")) {
+				    System.out.println(nl.item(k).getNodeName() + " " + nl.item(k).getTextContent());
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	    System.out.println("\n");
+	}
+    }
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException,
-            IOException, XPathExpressionException {
+	    IOException, XPathExpressionException {
 
-        XPathPrinter xprint = new XPathPrinter();
-        xprint.loadXML();
+	XPathPrinter xprint = new XPathPrinter();
+	xprint.loadXML();
 
-        try{
-	//printing all people in the list with detail
+	try {
+//	printing all people in the list with detail
 	    System.out.println("***** People in our Database *********\n");
 	    xprint.printPeople();
 	    System.out.println("***** End of People in our Database *********\n\n");
-	     
+//	     
 	    System.out.println("***** HealthProfile of person id 5 *********\n");
 	    xprint.printHealthProfile(5);
 	    System.out.println("***** End of HealthProfile of person id 5 *********\n\n");
-	       
-	       
+
 	    System.out.println("***** People with weight > 90 *********\n");
-		xprint.calcWeight(">90");
-		 System.out.println("***** End of People with weight > 90 *********");
-        
-	}catch(Exception e){
-		System.out.println(e);
-		}
+	    xprint.calcWeight(">90");
+	    System.out.println("***** End of People with weight > 90 *********");
+
+	} catch (Exception e) {
+	    System.out.println(e);
+	}
 
     }
 }
